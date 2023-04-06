4ead601593ff742caba9ee4d5b9ecff5aa36150f
Nikita Karamov <n.karamov@tu-braunschweig.de>
2023-04-06 11:03:52 +0200

    Init a Svelte app

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+// © 2023 Nikita Karamov <n.karamov@tu-braunschweig.de>
+//
+// SPDX-License-Identifier: EUPL-1.2 OR AGPL-3.0-only
+
+module.exports = {
+	root: true,
+	parser: "@typescript-eslint/parser",
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"prettier",
+	],
+	plugins: ["svelte3", "@typescript-eslint"],
+	ignorePatterns: ["*.cjs"],
+	overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
+	settings: {
+		"svelte3/typescript": () => require("typescript"),
+	},
+	parserOptions: {
+		sourceType: "module",
+		ecmaVersion: 2020,
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true,
+	},
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<!--
+© 2023 Nikita Karamov <n.karamov@tu-braunschweig.de>
+
+SPDX-License-Identifier: EUPL-1.2 OR AGPL-3.0-only
+-->
+<h1>Welcome to SvelteKit</h1>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
