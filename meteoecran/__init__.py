82460623f8fa8143550759ab677382bef90cd172
Nikita Karamov <n.karamov@tu-braunschweig.de>
2023-04-21 00:19:32 +0200

    Set locale in __init__

--- meteoecran/__init__.py
+++ meteoecran/__init__.py
@@ -2,7 +2,12 @@
 #
 # SPDX-License-Identifier: EUPL-1.2 OR AGPL-3.0-only
 
+import locale
+
 from meteoecran.web import app
+
+# TODO: set via settings
+locale.setlocale(locale.LC_ALL, "de_DE")
 
 __all__ = [
     "app",
